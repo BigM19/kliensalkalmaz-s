912d1ca5a14997dab93965feeb41a211b5c1ac74
BigM19 <nmatedan@gmail.com>
2024-04-22 23:20:28 +0200

    Update version

--- kliens_alkalmazas/kliens_alkalmazas/Form1.cs
+++ kliens_alkalmazas/kliens_alkalmazas/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Hotcakes.CommerceDTO.v1.Client;
+using Hotcakes.CommerceDTO.v1;
+using Hotcakes.CommerceDTO.v1.Orders;
+using Hotcakes.CommerceDTO.v1.Membership;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +11,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.UI.UserControls;
+using Newtonsoft.Json.Linq;
+using Hotcakes.Web;
+using Newtonsoft.Json;
+
+
 
 namespace kliens_alkalmazas
 {
@@ -15,6 +26,102 @@ namespace kliens_alkalmazas
         public Form1()
         {
             InitializeComponent();
+            this.BackColor = Color.FromArgb(237, 238, 233);
+            button1.BackColor = Color.FromArgb(115, 131, 145);
+            button1.ForeColor = Color.White;
+            button2.BackColor = Color.FromArgb(115, 131, 145);
+            button2.ForeColor = Color.White;
+            textBox1.BackColor = Color.FromArgb(195, 217, 185);
+            dateTimePicker1.Value = new DateTime(DateTime.Now.Year, 3, 5);
+            
+        }
+        private static Api apiHivas()
+        {
+            string url = "http://20.234.113.211:8081";
+            string kulcs = "1-96b39a7e-b4d5-4e33-ab50-b2176bfb9844";
+
+            Api proxy = new Api(url, kulcs);
+            return proxy;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Api proxy = apiHivas();
+            var s = proxy.CustomerAccountsCountOfAll().Content;
+            string regisztraltDarab = s.ToString();
+            textBox1.Text = regisztraltDarab;
+
+
+
+            var response = proxy.CustomerAccountsFindAll();
+
+            JObject jResponse = JObject.Parse(response.ObjectToJson());
+            JArray jArray = (JArray)jResponse["Content"];
+
+            string[] keysToRemove = {"Password", "Addresses", "Notes", "TaxExempt", "PricingGroupId", "FailedLoginCount", "LastUpdatedUtc", "ShippingAddress", "BillingAddress"};
+            foreach (JObject felhasznalo in jArray)
+            {
+                foreach (var key in keysToRemove.ToList())
+                {
+                    felhasznalo.Remove(key);
+                }
+            }
+
+            DataTable userTabla = (DataTable)JsonConvert.DeserializeObject(jArray.ToString(), typeof(DataTable));
+            dataGridView1.DataSource = userTabla;
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Api proxy = apiHivas();
+            var s = proxy.CustomerAccountsCountOfAll().Content;
+            string kiirom = s.ToString();
+            textBox1.Text = kiirom;
+
+
+
+            var response = proxy.CustomerAccountsFindAll();
+
+            JObject jResponse = JObject.Parse(response.ObjectToJson());
+            JArray jArray = (JArray)jResponse["Content"];
+
+            string[] keysToRemove = { "Password", "Addresses", "Notes", "TaxExempt", "PricingGroupId", "FailedLoginCount", "LastUpdatedUtc", "ShippingAddress", "BillingAddress" };
+            foreach (JObject felhasznalo in jArray)
+            {
+                foreach (var key in keysToRemove.ToList())
+                {
+                    felhasznalo.Remove(key);
+                }
+            }
+
+            DataTable userTabla = (DataTable)JsonConvert.DeserializeObject(jArray.ToString(), typeof(DataTable));
+
+            
+            DateTime fromDate = dateTimePicker1.Value.Date;
+            DateTime toDate = dateTimePicker2.Value.Date.AddDays(1).AddTicks(-1);
+
+            DataView dv = userTabla.DefaultView;
+            dv.RowFilter = $"LastLoginDateUtc >= '{fromDate}' AND LastLoginDateUtc <= '{toDate}'";
+            DataTable filteredTable = dv.ToTable();
+
+            dataGridView1.DataSource = filteredTable;
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            authentication azon = new authentication();
+            if (azon.ShowDialog() != DialogResult.OK) return;
+
+            Api proxy = apiHivas();
+            var customerID = azon.textBox3.Text;
+            ApiResponse<bool> response = proxy.CustomerAccountsDelete(customerID);
+
         }
     }
 }
---
+++ kliens_alkalmazas/kliens_alkalmazas/authentication.cs
@@ -0,0 +1,122 @@
+﻿using DotNetNuke.UI.Skins.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace kliens_alkalmazas
+{
+    public partial class authentication : Form
+    {
+        public authentication()
+        {
+            InitializeComponent();
+            this.BackColor = Color.FromArgb(237, 238, 233);
+            button1.BackColor = Color.FromArgb(115, 131, 145);
+            button1.ForeColor = Color.White;
+            button2.BackColor = Color.FromArgb(115, 131, 145);
+            button2.ForeColor = Color.White;
+            textBox1.BackColor = Color.FromArgb(195, 217, 185);
+            textBox2.BackColor = Color.FromArgb(195, 217, 185);
+            textBox3.BackColor = Color.FromArgb(195, 217, 185);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.ValidateChildren())
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        } 
+
+        private void textBox1_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(textBox1, "");
+            textBox1.BackColor = Color.LightGreen;
+        }
+
+        private void textBox2_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(textBox2, "");
+        }
+
+        private void textBox3_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(textBox3, "");
+            textBox3.BackColor = Color.LightGreen;
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            if (!CheckUser(textBox1.Text))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(textBox1, "A felhaználó hibás");
+                textBox1.BackColor = Color.LightSalmon;
+            }
+        }
+
+        private void textBox2_Validating(object sender, CancelEventArgs e)
+        {
+            if (!CheckPassword(textBox2.Text))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(textBox2, "A jelszó hibás");
+            }
+        }
+
+        private void textBox3_Validating(object sender, CancelEventArgs e)
+        {
+            if (!CheckId(textBox3.Text))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(textBox3, "Az Id nem lehet üres és/vagy számnak kell lennie");
+                textBox3.BackColor = Color.LightSalmon;
+            }
+        }
+
+        private bool CheckUser(string user)
+        {
+            return string.Equals(user, "admin", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private bool CheckPassword(string jelszo) 
+        {
+            return string.Equals(jelszo, "asd123", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private bool CheckId(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return false;
+            }
+
+            if (!int.TryParse(id, out _))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            textBox2.Text = String.Empty;
+            textBox2.PasswordChar = '*';
+        }
+    }
+}
